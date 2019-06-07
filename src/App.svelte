64150cfe02ed0784a9bae8456ca5be445cc5627e
Yi-Wei Chang <jchang6513@gmail.com>
2019-06-07 21:09:09 +0800

    update and remove todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,28 @@
 <script>
-	let todo = '';
-	let todos = [
-		{content: 'todo1', checked: true},
-		{content: 'todo2', checked: false},
-	];
+	import Store from './store.js';
 
 	let types = [
 		{text: 'All', value: 1},
 		{text: 'todo', value: 2},
 		{text: 'Accomplished', value: 3},
 	];
+
 	let type = {text: 'All', value: 1};
 
-	function addTodo(event) {
-		todos = todos.concat({content: todo, checked: false})
+	let todoTitle = ''
+	let todos = Store.isExist('todoList') ?
+		Store.get('todoList') :
+		[
+			{id: timeId(), title: 'todo1', checked: true},
+			{id: timeId(), title: 'todo2', checked: false}
+		];
+
+	function timeId() {
+		const trailing = 10**5;
+		return parseInt(trailing*(Date.now()+Math.random()))
 	}
 
+// todo type
 	function changType(event) {
 		const value = parseInt(event.target.value);
 		type = types.find(type => {
@@ -34,6 +41,38 @@
 		}
 	}
 
+// todo CRUD
+	function addTodo() {
+		todos = todos.concat({id: timeId(), title: todoTitle, checked: false});
+		todoTitle = '';
+		Store.set('todoList', todos);
+	}
+
+	function updateTodo(todo, value) {
+		const id = todo.id
+		todos = todos.map((todo) => {
+			if (todo.id === id) { todo = {...todo, ...value}; }
+			return todo;
+		})
+		Store.set('todoList', todos);
+	}
+
+	function removeTodo(todo) {
+		const id = todo.id;
+		let todoIndex = -1;
+		todos.find((todo, index) => {
+			if (todo.id === id) {
+				todoIndex = index;
+				return index;
+			}
+		})
+		if (todoIndex >= 0 ) {
+			todos.splice(todoIndex, 1);
+			Store.set('todoList', todos);
+			todos = todos;
+		}
+	}
+
 </script>
 
 <style>
@@ -47,7 +86,7 @@
 	<h1>todo List</h1>
 
 	<form class="mb-3" on:submit|preventDefault={addTodo}>
-		<input type="text" name="to-do" bind:value={todo}>
+		<input type="text" name="to-do" bind:value={todoTitle}>
 		<button>Submit</button>
 	</form>
 
@@ -63,6 +102,7 @@
 		</button>
 	{/each}
 
+
 	<div class="mt-3">
 		{#each todos as todo, index}
 			{#if isTypeOf(todo)}
@@ -71,16 +111,20 @@
 						id={`item-${index}`}
 						class="form-check-input"
 						type="checkbox"
-						value=""
-						bind:value={content}
 						checked={todo.checked}
+						on:click={() => updateTodo(todo,{checked: !todo.checked})}
 					>
 					<label
 						class="form-check-label"
 						for={`item-${index}`}
 					>
-						{todo.content}
+						{todo.title}
 					</label>
+					<button
+						on:click={() => removeTodo(todo)}
+					>
+						X
+					</button>
 				</div>
 			{/if}
 		{/each}
